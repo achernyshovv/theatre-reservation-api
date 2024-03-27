18fa802256830ad28a2ab1b3d504ee07875bc4c6
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-27 15:07:23 +0200

    Added registration of models in the admin interface

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Genre,
+    Actor,
+    Ticket,
+    Performance,
+    TheatreHall,
+    Reservation,
+    Play
+)
+
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Ticket)
+admin.site.register(Performance)
+admin.site.register(TheatreHall)
+admin.site.register(Reservation)
+admin.site.register(Play)
