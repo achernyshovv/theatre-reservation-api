1f6acea7459ddc904fec6b6639da0d2d8b1023ef
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-27 20:34:21 +0200

    modified admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -10,10 +10,32 @@ from .models import (
     Play
 )
 
+
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ("name", "rows", "seats_in_row")
+
+
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ('plays', 'theatre_hall', 'show_time')
+    list_filter = ('theatre_hall', 'show_time')
+
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at",)
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("row", "seat", "performance", "reservation")
+
+
+admin.site.register(Performance, PerformanceAdmin)
+admin.site.register(TheatreHall, TheatreHallAdmin)
 admin.site.register(Genre)
-admin.site.register(Actor)
-admin.site.register(Ticket)
-admin.site.register(Performance)
-admin.site.register(TheatreHall)
-admin.site.register(Reservation)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(Play)
