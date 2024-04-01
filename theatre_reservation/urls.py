082dabf9becc09e218c4add33a4197db7d6bdec0
Artem Chernyshov <achernyshovv1@gmail.com>
2024-04-01 06:46:21 +0200

    add url routes

--- theatre_reservation/urls.py
+++ theatre_reservation/urls.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("admin/", admin.site.urls),
+    path("api/cinema/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/cinema/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"),
+         name="swagger-ui"),
+    path(
+        "api/cinema/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
